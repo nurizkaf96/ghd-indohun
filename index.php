1f758b5fad521bf2331189a4ab3b27be653c2bf1
nurizkaf96 <nurizkaf96@gmail.com>
2018-05-03 12:00:52 +0700

    Update Gambar dari Nunu

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
     <a href="#programs" class="w3-bar-item w3-button w3-padding-large">Programs</a>
     <a href="#about" class="w3-bar-item w3-button w3-padding-large">About</a>
   </div>
-  <img class="w3-image" src="./img/headerimg.jpg" alt="Header Image">
+  <img class="w3-image" src="./img/hero/header.png" alt="Header Image">
 </header>
 
 <body>
@@ -77,7 +77,7 @@
       <h1><b>Programs</b></h1>
       <div class="w3-padding-large" id="padding-image">
         <div class="w3-third w3-margin-bottom padding-program-card">
-          <img src="./img/card1.jpg" style="width:100%; height: 15rem" class="w3-hover-opacity">
+          <img src="./img/program/executive.png" style="width:100%; height: 15rem" class="w3-hover-opacity">
           <div class="w3-container w3-white">
             <h3><b>Executive</b></h3>
             <p>High-level position of Health and International relation Professionals</p>
@@ -89,7 +89,7 @@
           </div>
         </div>
         <div class="w3-third w3-margin-bottom padding-program-card">
-          <img src="./img/card2.jpg" style="width:100%; height: 15rem" class="w3-hover-opacity">
+          <img src="./img/program/student.png" style="width:100%; height: 15rem" class="w3-hover-opacity">
           <div class="w3-container w3-white">
             <h3><b>Student</b></h3>
             <p>Design for University Student from second year and up</p>
@@ -101,7 +101,7 @@
           </div>
         </div>
         <div class="w3-third w3-margin-bottom padding-program-card">
-          <img src="./img/card1.jpg" style="width:100%; height: 15rem" class="w3-hover-opacity">
+          <img src="./img/program/intermediate.png" style="width:100%; height: 15rem" class="w3-hover-opacity">
           <div class="w3-container w3-white">
             <h3><b>Intermediate</b></h3>
             <p>High-level position of Health and International relation Professionals</p>
@@ -118,7 +118,7 @@
     <div id="contentExecutive" class="outer-container w3-hide w3-animate-opacity w3-bar-block">
       <div class="executive-training-container w3-row-padding">
         <div class="w3-third image-content-executive">
-          <img src="./img/card1.jpg" class="w3-hover-opacity">
+          <img src="./img/program/executive.png" class="w3-hover-opacity">
         </div>
         <div class="w3-twothird" id="inner-executive-training">
           <h1><b>EXECUTIVE TRAINING</b></h1>
@@ -229,12 +229,10 @@
     <div class="outer-container" id="training">
       <div id="training-container">
         <div class="w3-half" id="image-training">
-          <img src="./img/training.jpg" class="w3-hover-opacity">
+          <img src="./img/article/ghd-17.png" class="w3-hover-opacity">
         </div>
         <div class="w3-half" id="inner-training">
-          <h1><b>GHD</b></h1>
-          <h1><b>Training</b></h1>
-          <h1><b>2017</b></h1>
+          <h1><b>GHD Training 2017</b></h1>
           <h6>Diplomacy is undergoing profound changes in the 21st century – and global health is one of the areas where this is most apparent. The negotiation processes that shape and manage the global policy environment for health are increasingly conducted not only between sectors.</h6>
           <a href="./post.php">
             <button type="button" class="w3-button w3-red w3-margin-top">Learn More</button>
@@ -247,16 +245,14 @@
     <div class="outer-container" id="be-ourpartner">
       <div id="be-ourpartner-container">
         <div class="w3-half" id="inner-be-ourpartner">
-          <h1><b>Be</b></h1>
-          <h1><b>Our</b></h1>
-          <h1><b>Partner</b></h1>
+          <h1><b>Be Our Partner</b></h1>
           <h6>Are you the best candidate for our future Global Health Diplomacy Training? Join our Team as Speaker or Volunteer. Also, we are happy to open donate or grant from everyone, particularly Health Philanthropy for Global Health development.</h6>
           <a href="./post.php">
             <button type="button" class="w3-button w3-red w3-margin-top">Learn More</button>
           </a>
         </div>
         <div class="w3-half" id="image-be-ourpartner">
-          <img src="./img/bop.jpg" class="w3-hover-opacity">
+          <img src="./img/article/bop.png" class="w3-hover-opacity">
         </div>
       </div>
     </div>
@@ -314,13 +310,13 @@
         </div>
         <div class="w3-third w3-container w3-margin-bottom w3-center">
           <div class="w3-third w3-container w3-margin-bottom">
-            <img src="./img/usaid.png" alt="usaid" style="width: 8rem; height: 4rem;" class="w3-hover-opacity">
+            <img src="./img/partner/USAID.png" alt="usaid" style="height: 4rem;" class="w3-hover-opacity">
           </div>
           <div class="w3-third w3-container w3-margin-bottom">
-            <img src="./img/seaohun.png" alt="usaid" style="width: 6rem; height: 4rem;" class="w3-hover-opacity">
+            <img src="./img/partner/SEAOHUN.png" alt="usaid" style="height: 4rem;" class="w3-hover-opacity">
           </div>
           <div class="w3-third w3-container w3-margin-bottom">
-            <img src="./img/onehealth.png" alt="usaid" style="width: 8rem; height: 4rem;" class="w3-hover-opacity">
+            <img src="./img/partner/OHW.png" alt="usaid" style="height: 4rem;" class="w3-hover-opacity">
           </div>
         </div>
         <div class="w3-third w3-container w3-margin-bottom">
@@ -333,7 +329,7 @@
       <div class="w3-row-padding container-intouch">
         <div class="w3-third w3-container w3-margin-bottom" id="chat">
           <div class="w3-half">
-            <img src="./img/chat.png" style="width: 4rem">
+            <img src="./img/intouch/chat.png" style="width: 4rem">
           </div>
           <div class="w3-half">
             <p>847-939-9359</p>
@@ -341,7 +337,7 @@
         </div>
         <div class="w3-third w3-container w3-margin-bottom" id="messages">
           <div class="w3-half">
-            <img src="./img/message.png" style="width: 4rem">
+            <img src="./img/intouch/message.png" style="width: 4rem">
           </div>
           <div class="w3-half">
             <p>montreal@mail.com</p>
@@ -349,7 +345,7 @@
         </div>
         <div class="w3-third w3-container w3-margin-bottom" id="location">
           <div class="w3-half">
-            <img src="./img/globe.png" style="width: 4rem">
+            <img src="./img/intouch/globe.png" style="width: 4rem">
           </div>
           <div class="w3-half">
             <p>4116 Oak Avenue</br>Chicago, IL 60607</p>
