aff5ea7d3d4764c0987fddb8581f0b40d3e90c01
nurizkaf96 <nurizkaf96@gmail.com>
2018-05-05 11:58:20 +0700

    Konten dan Perbaikan Section News

--- index.php
+++ index.php
@@ -234,38 +234,39 @@
               <div class="w3-padding-large" id="padding-image">
                 <div class="w3-third w3-margin-bottom padding-news-card">
                   <div class="w3-container w3-white" id="text-news">
-                    <p>15 Feb 2018</p>
-                    <h3>Spotlight</h3>
-                    <p>Lorem ipsum To foster INDOHUN</p>
+                    <p class="news-date">3 May 2018</p>
+                    <h3>OHCC Throughout years...</h3>
+                    <p>One Health Collaboration Center (OHCC) Progress Throughout the Years—Insight for Future’s Development...</p>
                   </div>
-                  <img src="./img/program/executive.png" class="w3-hover-opacity">
+                  <img src="http://indohun.org/wp-content/uploads/2018/05/OHCC-Udayana-OH-Students-Club-3-Kelas-II.jpg" class="w3-hover-opacity">
                   <div class="w3-container w3-red">
-                    <a href="#" id="con3">
+                    <a href="http://indohun.org/news/one-health-collaboration-center-ohcc-progress-throughout-the-years-insight-for-futures-development/" id="con3">
                       <p>Learn More</p>
                     </a>
                   </div>
                 </div>
                 <div class="w3-third w3-margin-bottom padding-news-card">
                   <div class="w3-container w3-white" id="text-news">
-                    <p>15 Feb 2018</p>
-                    <h3><b>World</b></h3>
-                    <p>Lorem ipsum To foster INDOHUN sustainability, it is</p>
+                    <p class="news-date">26 April 2018</p>
+                    <h3>Prof Jeff Bender visited Indonesia</h3>
+                    <p>The Initiation of Travel Medicine in the World of One Health: ...</p>
                   </div>
-                  <img src="./img/program/student.png" class="w3-hover-opacity">
+                  <img src="http://indohun.org/wp-content/uploads/2018/04/Screen-Shot-2018-04-26-at-7.49.01-PM.png" class="w3-hover-opacity">
                   <div class="w3-container w3-red">
-                    <a href="#" id="con3">
+                    <a href="http://indohun.org/news/professor-jeff-bender-visits-to-indonesia/" id="con3">
                       <p>Learn More</p>
                     </a>
                   </div>
                 </div>
                 <div class="w3-third w3-margin-bottom padding-news-card">
                   <div class="w3-container w3-white" id="text-news">
-                    <p>15 Feb 2018</p>
-                    <h3><b>In The Media</b></h3>
+                    <p class="news-date">3 April 2018</p>
+                    <h3>Outbreak of Influenza in US ...</h3>
+                    <p>A Hundred Years after the 1918 Spanish Flu and We Are Now Experiencing ...</p>
                   </div>
-                  <img src="./img/program/intermediate.png" class="w3-hover-opacity">
+                  <img src="http://indohun.org/wp-content/uploads/2018/04/http-time.com3731745spanish-flu-history-.png" class="w3-hover-opacity">
                   <div class="w3-container w3-red">
-                    <a href="#" id="con3">
+                    <a href="http://indohun.org/news/a-hundred-years-after-the-1918-spanish-flu-and-we-are-now-experiencing-a-similar-thing-outbreak-of-influenza-in-the-united-states-2017-2018/" id="con3">
                       <p>Learn More</p>
                     </a>
                   </div>
@@ -279,38 +280,39 @@
               <div class="w3-padding-large" id="padding-image">
                 <div class="w3-third w3-margin-bottom padding-news-card">
                   <div class="w3-container w3-white" id="text-news">
-                    <p>15 Feb 2018</p>
-                    <h3><b>Spotlight</b></h3>
-                    <p>Lorem ipsum To foster INDOHUN</p>
+                    <p>19 Feb 2018</p>
+                    <h3><b>Deadly Measles Outbreak</b></h3>
+                    <p>Deadly Measles Outbreak and Malnutrition Striking Children in Asmat ...</p>
                   </div>
-                  <img src="./img/program/executive.png" class="w3-hover-opacity">
+                  <img src="http://indohun.org/wp-content/uploads/2018/02/5-1.png" class="w3-hover-opacity">
                   <div class="w3-container w3-red">
-                    <a href="#" id="con3">
+                    <a href="http://indohun.org/news/deadly-measles-outbreak-and-malnutrition-striking-children-in-asmat-regency-province-of-papua-the-role-of-the-indonesian-government/" id="con3">
                       <p>Learn More</p>
                     </a>
                   </div>
                 </div>
                 <div class="w3-third w3-margin-bottom padding-news-card">
                   <div class="w3-container w3-white" id="text-news">
-                    <p>15 Feb 2018</p>
-                    <h3><b>World</b></h3>
-                    <p>Lorem ipsum To foster INDOHUN sustainability, it is</p>
+                    <p>17 Feb 2018</p>
+                    <h3><b>Diphtheria Outbreak in Indonesia ...</b></h3>
+                    <p>Outbreak of diphtheria, a communicable disease known for its life-threatening risk ...</p>
                   </div>
-                  <img src="./img/program/student.png" class="w3-hover-opacity">
+                  <img src="http://indohun.org/wp-content/uploads/2018/02/JAK111-1211_2017_020155_hd.jpg" class="w3-hover-opacity">
                   <div class="w3-container w3-red">
-                    <a href="#" id="con3">
+                    <a href="http://indohun.org/news/an-old-foe-showing-its-fangs-diphtheria-outbreak-in-indonesia-2017/" id="con3">
                       <p>Learn More</p>
                     </a>
                   </div>
                 </div>
                 <div class="w3-third w3-margin-bottom padding-news-card">
                   <div class="w3-container w3-white" id="text-news">
-                    <p>15 Feb 2018</p>
-                    <h3><b>In The Media</b></h3>
+                    <p>See More</p>
+                    <h3><b>News and Updates</b></h3>
+                    <p>See more updates from Indohun</p>
                   </div>
-                  <img src="./img/program/intermediate.png" class="w3-hover-opacity">
+                  <img src="http://indohun.org/wp-content/themes/indohun/inc/images/photo/main-image.JPG" class="w3-hover-opacity">
                   <div class="w3-container w3-red">
-                    <a href="#" id="con3">
+                    <a href="http://indohun.org/news/" id="con3">
                       <p>Learn More</p>
                     </a>
                   </div>
